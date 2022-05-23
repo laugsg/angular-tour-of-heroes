f8bfdd9921d40bedb382d463d1ae75069e538cc1
laugsg <lautaro.grucci@joingsg.com>
2022-05-23 22:00:50 +0200

    update > async CRUD

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,45 +1,79 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 /** Get hero data
- * The HeroService could get hero data 
+ * The HeroService could get hero data
  * from anywhere —a web service, local storage, or a mock data source.
- * Removing data access from components means you can change your mind 
- * about the implementation anytime, without touching any components. 
+ * Removing data access from components means you can change your mind
+ * about the implementation anytime, without touching any components.
  * They don't know how the service works.
  */
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
+
   /**
-   * This is a typical "service-in-service" scenario: 
-   * MessageService is injected into the HeroService 
+   * This is a typical "service-in-service" scenario:
+   * MessageService is injected into the HeroService
    * which is injected into the HeroesComponent.
    */
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   /** getHero
+   * GET hero by id. Will 404 if id not found
    * @returns {Observable<Hero[]>}
-   * @NOTE:
-   * For now, assume that a hero with the specified `id` always exists.
-   * Error handling will be added in the next step of the tutorial.
    */
-  getHero(id:Number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero)
+  getHero(id: Number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      )
   }
 
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
